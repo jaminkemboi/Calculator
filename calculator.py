70df0b21575e43181fab2d4f33c2a712a6383b7f
Jamin Kiplagat Kemboi <46446644+jaminkemboi@users.noreply.github.com>
2021-01-17 17:39:28 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#Basic Calculator
+
+#First variable that takes first number
+num1 = float(input("Enter first number: "))
+
+#Second variable that takes second number
+num2 = float(input("Enter second number: "))
+
+#Variable that is the answer and adds both numbers
+result = num1 + num2
+
+#Finally result 
+print(result)
